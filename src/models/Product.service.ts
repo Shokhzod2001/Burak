65459e394fa839b36a2b34308b2e6bb4f72cbb70
Shokhzod2001 <shahzodabdumalikov1@gmail.com>
2025-04-02 23:42:44 +0900

    feat: createNewProduct business logic by restaurant member

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -1,9 +1,23 @@
+import { Product, ProductInput } from "../libs/types/product";
+import Errors, { HttpCode, Message } from "./../libs/Errors";
 import ProductModel from "../schema/Product.model";
 
 class ProductService {
   private readonly productModel;
   constructor() {
     this.productModel = ProductModel;
+  }
+
+  //SPA
+
+  //BSSR
+  public async createNewProduct(input: ProductInput): Promise<Product> {
+    try {
+      return await this.productModel.create(input);
+    } catch (err) {
+      console.error("Error, model:createNewProduct:", err);
+      throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+    }
   }
 }
 export default ProductService;
