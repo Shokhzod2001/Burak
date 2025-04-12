3b408ac4e534335bfb59a37db94f5b746e87dd4b
Shokhzod2001 <shahzodabdumalikov1@gmail.com>
2025-04-12 15:46:00 +0900

    fix: admin project refactoring

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -25,8 +25,6 @@ $(function () {
   $(".new-product-status").on("change", async function (e) {
     const id = e.target.id;
     const productStatus = $(`#${id}.new-product-status`).val();
-    console.log("id:", id);
-    console.log("productStatus:", productStatus);
 
     try {
       const response = await axios.post(`/admin/product/${id}`, {
@@ -46,12 +44,12 @@ $(function () {
 });
 
 function validateForm() {
-  const productName = $(".product-name").val();
-  const productPrice = $(".product-price").val();
-  const productLeftCount = $(".product-left-count").val();
-  const productCollection = $(".product-collection").val();
-  const productDesc = $(".product-desc").val();
-  const productStatus = $(".product-status").val();
+  const productName = $(".product-name").val(),
+    productPrice = $(".product-price").val(),
+    productLeftCount = $(".product-left-count").val(),
+    productCollection = $(".product-collection").val(),
+    productDesc = $(".product-desc").val(),
+    productStatus = $(".product-status").val();
 
   if (
     productName === "" ||
@@ -68,11 +66,10 @@ function validateForm() {
 
 function previewFileHandler(input, order) {
   const imgClassName = input.className;
-  console.log("input:", input);
 
-  const file = $(`.${imgClassName}`).get(0).files[0];
-  const fileType = file["type"];
-  const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+  const file = $(`.${imgClassName}`).get(0).files[0],
+    fileType = file["type"],
+    validImageType = ["image/jpg", "image/jpeg", "image/png"];
 
   if (!validImageType.includes(fileType)) {
     alert("Please insert only jpeg, jpg and png!");
